fe9c77796c4e28e9502954b6239a3fcb80e1cba5
taofeeekishola <taofeekishola1791@gmail.com>
2025-03-05 04:09:48 +0100

    endpoint to create agent account has been created in agents-account services

--- src/agents-account/agents-account.controller.ts
+++ src/agents-account/agents-account.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AgentsAccountService } from './agents-account.service';
+import { CreateAgentAccountDto } from './dto/create-agentsAccount.dto';
 
 @Controller('agents-account')
-export class AgentsAccountController {}
+export class AgentsAccountController {
+
+    constructor(private readonly agentsAccountServices: AgentsAccountService){}
+
+    @Post()
+    create(@Body() createAgentAccountDto: CreateAgentAccountDto) {
+        return this.agentsAccountServices.create(createAgentAccountDto);
+    }
+
+}
--- src/agents-account/agents-account.module.ts
+++ src/agents-account/agents-account.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AgentsAccountService } from './agents-account.service';
 import { AgentsAccountController } from './agents-account.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Agent } from 'src/agents/entities/agent.entity';
+import { AgentAccount } from './entities/agentAccount.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AgentAccount, Agent])],
   providers: [AgentsAccountService],
   controllers: [AgentsAccountController]
 })
